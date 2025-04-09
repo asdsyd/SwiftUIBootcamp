be73489a97f2ab3384a24779a4c500c677603fd1
Asad Sayeed <22dp2000212@ds.study.iitm.ac.in>
2025-04-09 14:36:39 +0530

    Initial Commit

---
+++ SwiftUIBootcamp/SwiftUIBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcampApp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Asad Sayeed on 09/04/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
