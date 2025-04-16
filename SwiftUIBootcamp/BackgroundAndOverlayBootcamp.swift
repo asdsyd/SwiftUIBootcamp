98bda827fd129725945edab38b75f1041de46bb5
Asad Sayeed <22dp2000212@ds.study.iitm.ac.in>
2025-04-17 01:12:56 +0530

    Backgrounds and Overlays in SwiftUI | Bootcamp #9

--- SwiftUIBootcamp/BackgroundAndOverlayBootcamp.swift
+++ SwiftUIBootcamp/BackgroundAndOverlayBootcamp.swift
@@ -9,19 +9,32 @@ import SwiftUI
 
 struct BackgroundAndOverlayBootcamp: View {
     var body: some View {
-        Rectangle()
-            .frame(width: 100, height: 100)
-            .foregroundStyle(.gray)
-            .overlay(alignment: .topLeading) {
-                Rectangle()
-                    .fill(.blue)
-                    .frame(width: 50, height: 50)
-            }
+        Image(systemName: "heart.fill")
+            .font(.system(size: 50))
+            .foregroundStyle(.white)
+            .shadow(color: Color.gray, radius: 5, x: 0, y: 0)
             .background(
-                Rectangle()
-                    .fill(.purple)
-                    .frame(width: 150, height: 150)
-                , alignment: .bottomTrailing
+                Circle()
+                    .fill(
+                        LinearGradient(colors: [Color( #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)),Color( #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)) ],
+                                       startPoint: .topLeading,
+                                       endPoint: .bottomTrailing)
+                    )
+                    .frame(width: 100, height: 100)
+                    .shadow(color: Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)), radius: 10, x: 0, y: 10)
+                    .overlay(
+                        Circle()
+                            .fill(.blue)
+                            .frame(width: 35, height: 35)
+                            .overlay(
+                                Text("5")
+                                    .font(.headline)
+                                    .foregroundStyle(.white)
+                            )
+                            .shadow(color: .purple, radius: 10, x: 5, y: 5)
+                        , alignment: .bottomTrailing
+                    )
+                
             )
     }
 }
