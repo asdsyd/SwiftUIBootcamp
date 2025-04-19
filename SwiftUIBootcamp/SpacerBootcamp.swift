313f0af0e1a8158c3d52ed2e8e654087baf16aea
Asad Sayeed <22dp2000212@ds.study.iitm.ac.in>
2025-04-19 15:22:55 +0530

    How to use Spacer in SwiftUI | Bootcamp #12

--- SwiftUIBootcamp/SpacerBootcamp.swift
+++ SwiftUIBootcamp/SpacerBootcamp.swift
@@ -9,39 +9,30 @@ import SwiftUI
 
 struct SpacerBootcamp: View {
     var body: some View {
-        HStack(spacing: 0) {
-            Spacer(minLength: 0)
-                .frame(height: 10)
-                .background(.orange)
-
-            
-            Rectangle()
-                .frame(width: 50, height: 50)
+        
+        VStack {
+            HStack(spacing: 0) {
+                Image(systemName: "xmark")
+                
+                Spacer()
+                    .frame(height: 10)
+                    .background(.orange)
+                
+                Image(systemName: "gear")
+            }
+            .font(.title)
+//            .background(.yellow)
+            .padding(.horizontal)
+//            .background(.blue)
             
             Spacer()
-                .frame(height: 10)
+                .frame(width: 10)
                 .background(.orange)
-
-            Rectangle()
-                .fill(.red)
-                .frame(width: 50, height: 50)
             
-            Spacer()
-                .frame(height: 10)
-                .background(.orange)
-
             Rectangle()
-                .fill(.green)
-                .frame(width: 50, height: 50)
-
-            Spacer(minLength: 0)
-                .frame(height: 10)
-                .background(.orange)
-
+                .frame(height: 55)
         }
-        .background(.yellow)
-//        .padding(.horizontal, 200)
-        .background(.blue)
+//        .background(.yellow)
     }
 }
 
