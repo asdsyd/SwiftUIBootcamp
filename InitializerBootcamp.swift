8abbae63c3d48b6b4e8e90a7ea5922697e48b920
Asad Sayeed <22dp2000212@ds.study.iitm.ac.in>
2025-04-19 16:16:17 +0530

    How to use inits and enums in SwiftUI | Bootcamp #13

---
+++ InitializerBootcamp.swift
@@ -0,0 +1,55 @@
+//
+//  InitializerBootcamp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Asad Sayeed on 19/04/25.
+//
+
+import SwiftUI
+
+struct InitializerBootcamp: View {
+    
+    let backgroundColor: Color
+    let count: Int
+    let title: String
+    
+    init(count: Int, fruit: Fruit) {
+        self.count = count
+        
+        if fruit == .apple {
+            self.title = "Apples"
+            self.backgroundColor = .red
+        } else {
+            self.title = "Oranges"
+            self.backgroundColor = .orange
+        }
+    }
+    
+    enum Fruit {
+        case apple
+        case orange
+    }
+    
+    var body: some View {
+        VStack(spacing: 12) {
+            Text("\(count)")
+                .font(.largeTitle)
+                .foregroundStyle(.white)
+                .underline()
+
+            Text("\(title)")
+                .font(.headline)
+                .foregroundStyle(.white)
+        }
+        .frame(width: 150, height: 150)
+        .background(backgroundColor)
+        .cornerRadius(10)
+    }
+}
+
+#Preview {
+    HStack {
+        InitializerBootcamp(count: 100, fruit: .apple)
+        InitializerBootcamp(count: 99, fruit: .orange)
+    }
+}
