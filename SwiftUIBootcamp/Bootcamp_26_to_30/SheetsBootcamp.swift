928634046b5ee2a7ecaf8f1e4618d192ccba978b
Asad Sayeed <22dp2000212@ds.study.iitm.ac.in>
2025-04-22 20:00:10 +0530

    Display pop-up Sheets and FullScreenCovers in SwiftUI | Bootcamp #28

---
+++ SwiftUIBootcamp/Bootcamp_26_to_30/SheetsBootcamp.swift
@@ -0,0 +1,67 @@
+//
+//  SheetsBootcamp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Asad Sayeed on 22/04/25.
+//
+
+import SwiftUI
+
+struct SheetsBootcamp: View {
+    
+    @State var showSheet: Bool = false
+    
+    var body: some View {
+        ZStack {
+            Color.green
+                .ignoresSafeArea()
+            
+            Button {
+                showSheet.toggle()
+            } label: {
+                Text("Button")
+                    .foregroundStyle(.green)
+                    .font(.headline)
+                    .padding(5)
+                    .padding(.horizontal , 20)
+                    .background(Color.white.cornerRadius(20))
+            }
+//            .fullScreenCover(isPresented: $showSheet, content: {
+//                SecondScreen()
+//            })
+            .sheet(isPresented: $showSheet) {
+                SecondScreen()
+            }
+        }
+    }
+}
+
+struct SecondScreen: View {
+    
+//    @Environment(\.presentationMode) var presentationMode
+    @Environment(\.dismiss) var dismissScreen
+    
+    var body: some View {
+        ZStack(alignment: .topLeading) {
+            Color.red
+                .ignoresSafeArea()
+            
+            Button {
+//                presentationMode.wrappedValue.dismiss()
+                dismissScreen()
+            } label: {
+                Image(systemName: "xmark")
+                    .foregroundStyle(.red)
+                    .font(.largeTitle)
+                    .padding()
+                    .background(Color.white.cornerRadius(150))
+                    .padding()
+            }
+        }
+    }
+}
+
+#Preview {
+    SheetsBootcamp()
+//    SecondScreen()
+}
