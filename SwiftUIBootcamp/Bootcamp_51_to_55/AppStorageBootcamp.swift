35bb9436e998adb6b15c101d96ec411a0d1db3bc
Asad Sayeed <22dp2000212@ds.study.iitm.ac.in>
2025-05-02 06:49:35 +0530

    How to use @AppStorage in SwiftUI | Bootcamp #52

---
+++ SwiftUIBootcamp/Bootcamp_51_to_55/AppStorageBootcamp.swift
@@ -0,0 +1,37 @@
+//
+//  AppStorageBootcamp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Asad Sayeed on 02/05/25.
+//
+
+import SwiftUI
+
+struct AppStorageBootcamp: View {
+    
+//    @State var currentUserName: String?
+    @AppStorage("name") var currentUserName: String?
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            Text(currentUserName ?? "Add Name Here")
+            
+            if let name = currentUserName {
+                Text(name)
+            }
+            
+            Button("Save".uppercased()) {
+                let name = "Sera"
+                currentUserName = name
+//                UserDefaults.standard.set(name, forKey: "name")
+            }
+        }
+//        .onAppear {
+//            currentUserName = UserDefaults.standard.string(forKey: "name")
+//        }
+    }
+}
+
+#Preview {
+    AppStorageBootcamp()
+}
