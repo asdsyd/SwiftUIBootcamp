1068e5d3d571526ccb531a1ccc98d4c9f7490448
Asad Sayeed <22dp2000212@ds.study.iitm.ac.in>
2025-05-03 19:34:55 +0530

    How to select text with TextSelection in SwiftUI | Bootcamp #56

---
+++ SwiftUIBootcamp/Bootcamp_56_to_60/TextSelectionBootcamp.swift
@@ -0,0 +1,19 @@
+//
+//  TextSelectionBootcamp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Asad Sayeed on 03/05/25.
+//
+
+import SwiftUI
+
+struct TextSelectionBootcamp: View {
+    var body: some View {
+        Text("Hello, World!")
+            .textSelection(.enabled)
+    }
+}
+
+#Preview {
+    TextSelectionBootcamp()
+}
