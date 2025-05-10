6a5094579825ac7f508f06345e9339bde487bc6c
Asad Sayeed <22dp2000212@ds.study.iitm.ac.in>
2025-05-10 19:47:44 +0530

    How to use Menu in SwiftUI | Bootcamp #68

---
+++ SwiftUIBootcamp/Bootcamp_66_to_70/MenuBootcamp.swift
@@ -0,0 +1,33 @@
+//
+//  MenuBootcamp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Asad Sayeed on 10/05/25.
+//
+
+import SwiftUI
+
+struct MenuBootcamp: View {
+    var body: some View {
+        Menu("Click me!") {
+            Button("One") {
+                
+            }
+            Button("Two") {
+                
+            }
+            Button("Three") {
+                
+            }
+            Button("Four") {
+                
+            }
+
+
+        }
+    }
+}
+
+#Preview {
+    MenuBootcamp()
+}
